6ec7a249b5eb38c0fa43b1dcbba52996808a882b
rintarotajima <rintarotajima@gmail.com>
2025-04-11 23:26:12 +0900

    style: 数字ボタンのサイズ変更

--- app/src/main/java/com/example/memocalculator/MainActivity.kt
+++ app/src/main/java/com/example/memocalculator/MainActivity.kt
@@ -5,18 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -27,7 +22,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontVariation.weight
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -188,7 +182,6 @@ fun CalculationField(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(100.dp)
             .background(Color.LightGray)
             .padding(2.dp),
         contentAlignment = Alignment.BottomEnd
@@ -210,13 +203,13 @@ fun CalculationButton(
     Button(
         onClick = onClick,
         modifier = modifier
-//            .fillMaxHeight()
-            .size(60.dp)
+            .size(132.dp)
             .padding(4.dp)
     ) {
         Text(
             text = label,
             fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
         )
     }
@@ -231,7 +224,6 @@ fun CalculationAppPreview() {
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
-//                    .background(Color.LightGray)
             )
         }
     }
